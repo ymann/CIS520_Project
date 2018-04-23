a6b0b83944ddf54e77077437890a41fe0fe9a5ba
ymann <ymann@wharton.upenn.edu>
2018-04-22 20:03:42 -0400

    Scaler

--- nn.py
+++ nn.py
@@ -22,3 +22,11 @@ xl = no_labels_data[1:15222,:]
 xul = no_labels_data[15222:23897,:]
 
 X_train, X_test, y_train, y_test = train_test_split(xl, y)
+
+
+# Normalize data
+scaler = StandardScaler()  
+scaler.fit(X_train)  
+X_train = scaler.transform(X_train)  
+
+X_test = scaler.transform(X_test)  
