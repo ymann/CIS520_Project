334891e8d57c1dfc8f4aae5dfa2732fd8bd8fb33
ymann <ymann@wharton.upenn.edu>
2018-04-22 20:03:10 -0400

    Neural nets data processing

---
+++ nn.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.neural_network import MLPClassifier as mlp
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.model_selection import train_test_split
+
+
+np.set_printoptions(threshold=np.inf)
+
+data = np.genfromtxt('all_gun_data.csv',delimiter=',')
+
+no_labels_data = np.copy(data)
+no_labels_data = np.delete(no_labels_data, 15, axis=1)
+
+y = data[:,15]
+y = y[1:15222]
+
+
+xl = no_labels_data[1:15222,:]
+xul = no_labels_data[15222:23897,:]
+
+X_train, X_test, y_train, y_test = train_test_split(xl, y)
