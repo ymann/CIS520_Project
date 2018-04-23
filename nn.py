f652c886b735b24acafbff28423113c53acf0123
ymann <ymann@wharton.upenn.edu>
2018-04-22 20:04:50 -0400

    Finished model and added error rates

--- nn.py
+++ nn.py
@@ -32,3 +32,17 @@ X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)  
 
 clf = mlp(solver='adam', hidden_layer_sizes=(30, 30), alpha=.00001)
+
+clf.fit(X_train, y_train)                         
+predictions = clf.predict(X_test)
+num_correct_intentional_shootings = np.sum(predictions[y_test == 1] == 1)
+num_intentional_shootings = np.sum(y_test == 1)
+intentional_shooting_error_rate = 1 - num_correct_intentional_shootings / num_intentional_shootings
+
+num_errors = np.sum(y_test != predictions)
+num_instances = len(predictions)
+error_rate = num_errors / num_instances
+
+print("False negative rate: " + str(intentional_shooting_error_rate))
+print("Error rate: " + str(error_rate))
+
